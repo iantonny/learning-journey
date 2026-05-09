b31b184f173552ba8930bfa3e606db1ea2078a97
Ian Mungai <ianmungai700@gmail.com>
2026-05-09 22:48:09 +0300

    [week3] feat(recursion): draw pyramid using recursive function — base case and stack unwinding
    Implement recursive function to draw a pyramid.

---
+++ week3/recursion_pyramid.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void draw(int height);
+
+int main(void)
+{
+    int height = get_int("Height:");
+
+    draw(height);
+
+}
+void draw(int height)
+{
+
+    //base case
+    if(height <= 0)
+    {
+        return;
+    }
+      draw(height-1);
+
+for(int i = 0; i <height; i++)
+{
+    printf("#");
+}
+printf("\n");
+
+}
